b8343cdc6302cd4cd735fe080de4fe1ef74d54ee
nenshikalathiya <kalathiyanenshi@gmai.com>
2024-10-18 00:50:15 -0400

    UI Update

--- app/src/main/java/com/example/hcahealthcaretask/application/GithubApplication.kt
+++ app/src/main/java/com/example/hcahealthcaretask/application/GithubApplication.kt
@@ -8,6 +8,7 @@ class GithubApplication : Application(){
 
     //to define some object scope are on application level
     lateinit var appComponent: ApplicationComponent
+
     override fun onCreate() {
         super.onCreate()
       //Suppose to use dagger dependency injection but having problem to generate dagger classes
